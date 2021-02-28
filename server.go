e66f335200d6694ff8f1604bd37fb67cc57f9a68
Changwei Tu <changweitu@gmail.com>
2021-02-28 11:30:04 +0800

    send message, rename user name, notice user that his has been online/offline, close connect when timeout

--- server.go
+++ server.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net"
 	"sync"
+	"time"
 )
 
 type Server struct {
@@ -16,13 +18,41 @@ type Server struct {
 
 
 //处理客户端请求
-func (s *Server) handle(conn net.Conn) {
-	user := NewUser(conn)
-	s.Locker.Lock()
-	s.OnlineUsers[user.Name] = user
-	s.Locker.Unlock()
-	s.broadcastMessage(user, "已上线")
-	select { }
+func (s *Server) handleConnection(conn net.Conn) {
+	user := NewUser(conn, s)
+	user.Online()
+	isLive := make(chan bool)
+	go func() {
+		buf := make([]byte, 4096)
+		for {
+			n, err := conn.Read(buf)
+			if n == 0 {
+				user.Offline()
+				return
+			}
+			if err != nil && err != io.EOF {
+				fmt.Println("Conn Read error:", err)
+				return
+			}
+			msg := string(buf[:n-1])
+			user.HandleMessage(msg)
+			isLive <- true
+		}
+	}()
+	for {
+		select {
+		case <-isLive:
+		case <- time.After(10 * time.Second):
+			user.SendMsg("你被踢了")
+			s.Locker.Lock()
+			defer s.Locker.Unlock()
+			delete(s.OnlineUsers, user.Name)
+			close(user.Message)
+			conn.Close()
+			return
+		}
+
+	}
 }
 func (s *Server) broadcastMessage(user *User, msg string)  {
 	s.Message<- "[" + user.Name + "]:" + msg
@@ -56,7 +86,7 @@ func (s *Server) Start() {
 		//当有用户上线时，广播消息。
 		go s.listenMessage()
 		//为每个客户端连接开辟协程，处理请求
-		go s.handle(conn)
+		go s.handleConnection(conn)
 	}
 }
 //创建服务器
--- user.go
+++ user.go
@@ -7,7 +7,9 @@ type User struct {
 	Addr string
 	Message chan string
 	conn net.Conn
+	server *Server
 }
+
 //监听消息，一旦有消息，则传给客户端
 func (u *User)listenMessage()  {
 	for {
@@ -15,9 +17,52 @@ func (u *User)listenMessage()  {
 		u.conn.Write([]byte(msg + "\n"))
 	}
 }
-func NewUser(conn net.Conn) *User {
+
+func (u *User) SendMsg(msg string)  {
+	u.conn.Write([]byte(msg))
+}
+
+func (u *User) HandleMessage(msg string) {
+	if msg == "who" {//返回当前在线用户
+		u.server.Locker.Lock()
+		for _, user := range u.server.OnlineUsers {
+			msg := "[" + user.Addr + "]" + user.Name + ":" + "在线...\n"
+			u.SendMsg(msg)
+		}
+		u.server.Locker.Unlock()
+	} else if len(msg) > 7 && msg[:7]=="rename|"  {//重命名，格式为"rename|jack"
+		name := msg[7:]
+		u.server.Locker.Lock()
+		_, ok := u.server.OnlineUsers[name]
+		if ok {
+			u.SendMsg("用户名已存在")
+		} else {
+			delete(u.server.OnlineUsers, name)
+			u.server.OnlineUsers[name] = u
+			u.Name = name
+			u.SendMsg("用户名已修改为：" + u.Name)
+		}
+		u.server.Locker.Unlock()
+
+	} else {
+		u.server.broadcastMessage(u, msg)
+	}
+}
+func (u *User) Online()  {
+	u.server.Locker.Lock()
+	u.server.OnlineUsers[u.Name] = u
+	u.server.Locker.Unlock()
+	u.server.broadcastMessage(u, "已上线")
+}
+func (u *User) Offline() {
+	u.server.Locker.Lock()
+	delete(u.server.OnlineUsers, u.Name)
+	u.server.Locker.Unlock()
+	u.server.broadcastMessage(u, "下线")
+}
+func NewUser(conn net.Conn, server *Server) *User {
 	addr := conn.RemoteAddr().String()
-	user := &User{addr, addr, make(chan string), conn}
+	user := &User{addr, addr, make(chan string), conn, server}
 	go user.listenMessage()
 	return user
 }
